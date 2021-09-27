dec687ff4a4e46365897d2fa22e6a8539012ba17
Ajri Sylhasi <ajri@hennessey.com>
2021-09-27 13:52:28 +0200

    Pushed clicked version

--- src/index.js
+++ src/index.js
@@ -3,6 +3,65 @@ var operators = ["+", "-", "/", "*", "s", "%"];
 var chosenOperator = "";
 var firstNumber = 0;
 var secondNumber = 0;
+var addChar = function (e) {
+    var first = document.getElementById('first');
+    var second = document.getElementById('second');
+    var result = document.getElementById('result');
+    var display = document.getElementById('display');
+    e = e || window.event;
+    if(/\d/.test(e)) {
+        if(state == 0 && first.innerHTML.length < 14) {
+            first.innerHTML += e;
+            display.classList.remove("result");
+            display.classList.add("first");
+        }
+        if(state == 1 && second.innerHTML.length < 14) {
+            document.getElementById('second').innerHTML += e;
+            display.classList.remove("first");
+            display.classList.add("second");
+        }
+    }  else if(e == "C") {
+        chosenOperator = "";
+        first.innerHTML = "";
+        second.innerHTML = "";
+    } else if( operators.indexOf(e) > -1) {
+        if(e == "s") {
+            state = 2;
+            chosenOperator = e;
+        } else {
+            state = 1;
+            chosenOperator = e;
+        }
+    } else if((state == 1 || state == 2) && (e == "=" || e == "Enter")) {
+        state = 0;
+        firstNumber = parseInt(first.innerHTML, 10);
+        secondNumber = parseInt(second.innerHTML, 10);
+        if(chosenOperator == "+") {
+            resultCalc = firstNumber + secondNumber;
+        } else if(chosenOperator == "-") {
+            resultCalc = firstNumber - secondNumber;
+        } else if(chosenOperator == "*") {
+            resultCalc = firstNumber * secondNumber;
+        } else if(chosenOperator == "/") {
+            resultCalc = firstNumber / secondNumber;
+        } else if(chosenOperator == "%") {
+            resultCalc = firstNumber/secondNumber*100;
+        } else if(chosenOperator == "s") {
+            resultCalc = Math.sqrt(firstNumber);
+        } else if(chosenOperator == "MOD") {
+            resultCalc = firstNumber % secondNumber;
+        }else {
+            resultCalc = 0;
+        }
+        chosenOperator = "";
+        first.innerHTML = "";
+        second.innerHTML = "";
+        result.innerHTML = resultCalc;
+        display.classList.remove("second");
+        display.classList.remove("first");
+        display.classList.add("result");
+    }
+}
 document.onkeypress = function (e) {
     var first = document.getElementById('first');
     var second = document.getElementById('second');
@@ -41,7 +100,7 @@ document.onkeypress = function (e) {
         } else if(chosenOperator == "/") {
             resultCalc = firstNumber / secondNumber;
         } else if(chosenOperator == "%") {
-            resultCalc = firstNumber % secondNumber;
+            resultCalc = firstNumber/secondNumber*100;
         } else if(chosenOperator == "s") {
             resultCalc = Math.sqrt(firstNumber);
         } else if(chosenOperator == "MOD") {
@@ -57,6 +116,4 @@ document.onkeypress = function (e) {
         display.classList.remove("first");
         display.classList.add("result");
     }
-    
 }
-
